de79476065ef6e3bce814658de9a6a2a04a98147
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-23 22:20:52 +0530

    Pyaudio practice

---
+++ load_mp3.py
@@ -0,0 +1,11 @@
+from pydub import AudioSegment
+audio=AudioSegment.from_wav("output.wav")
+# increase the volumne by 6dB
+
+audio = audio+6
+audio=audio*2
+
+audio=audio.fade_in(2000)
+
+audio.export("mashup.mp3",format="mp3")
+audio2=AudioSegment.from_mp3("mashup.mp3")
